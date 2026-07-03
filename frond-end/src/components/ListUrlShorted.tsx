577f2e753234290bf1112f6abca04fbdc03dfb42
frdiskandr <fariditb159@gmail.com>
2026-07-04 00:17:53 +0700

    delete button trash

--- frond-end/src/components/ListUrlShorted.tsx
+++ frond-end/src/components/ListUrlShorted.tsx
@@ -92,11 +92,11 @@ const ListUrlShorted: React.FC<ListUrlShortedProps> = ({ items = [], onDelete })
                       </Button>
                     </div>
 
-                    <div className="col-span-5 truncate text-xs text-start" title={it.original_url}>
+                    <div className="col-span-5 truncate text-xs text-center" title={it.original_url}>
                       {it.original_url}
                     </div>
 
-                    <div className="col-span-1 flex justify-end">
+                    {/* <div className="col-span-1 flex justify-end">
                       <Button size="icon" variant="destructive" className="p-1" onClick={() => handleDelete(it.id)} aria-label="Delete">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block">
                           <polyline points="3 6 5 6 21 6" />
@@ -105,7 +105,7 @@ const ListUrlShorted: React.FC<ListUrlShortedProps> = ({ items = [], onDelete })
                           <path d="M9 6V4a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2" />
                         </svg>
                       </Button>
-                    </div>
+                    </div> */}
                   </div>
                 ))
               )}
